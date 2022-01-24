111197ece87451fe607fe1c70a22b588e162e032
Tgarrey37 <tpopp0303@gmail.com>
2022-01-23 19:48:32 -0700

    Finished product

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,7 +48,7 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   try {
-  const categoryData = await Category.update({
+  const categoryData = await Category.update(req.body,{
     where: {
       id: req.params.id
     },
@@ -67,7 +67,7 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-    const categoryRoutes = await Category.Destroy({
+    const categoryRoutes = await Category.destroy({
       where: {
         id: req.params.id,
       },
